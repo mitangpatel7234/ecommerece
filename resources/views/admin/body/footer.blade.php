a1a37de12d107963eaf3d857569cd7ef62d2a8f1
mitangpatel7234 <mitangpatel7234@gmail.com>
2022-08-03 23:52:57 +0530

    first commit

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="https://mitang1803.herokuapp.com/ " target=”_blank”>https://mitang1803.herokuapp.com/</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2021 <a href="#">Mitang Patel</a>. All Rights Reserved.
+  </footer>
